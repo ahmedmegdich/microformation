ccaba2af44d05cea86be9c6c21bb91a8652caa5b
ahmedmegdich <148362059+ahmedmegdich@users.noreply.github.com>
2024-12-04 10:56:30 +0000

    yesl

--- registry-server/src/test/java/com/example/registry_server/RegistryServerApplicationTests.java
+++ registry-server/src/test/java/com/example/registry_server/RegistryServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.registry_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class RegistryServerApplicationTests {
 
 	@Test
 	void contextLoads() {
